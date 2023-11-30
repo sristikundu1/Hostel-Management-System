e81883df660fadbb951fc4871ec933adadbe2316
sristikundu1 <nidhukundu1234@gmail.com>
2023-11-30 23:50:13 +0600

    add some data in json file

--- src/Pages/Home/OrderTab/OrderTab.jsx
+++ src/Pages/Home/OrderTab/OrderTab.jsx
@@ -3,7 +3,7 @@ import CardMeal from '../MealCard/CardMeal';
 
 const OrderTab = ({items}) => {
     return (
-        <div className='grid grid-cols-3 gap-7 mt-10'>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7 mt-10'>
              {
                         items.map(item => <CardMeal
                         key={item.id}
